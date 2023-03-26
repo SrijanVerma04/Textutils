80885b22b1d1aa2308f150deca85d58b50013021
Srijan Verma <srijan@Srijans-MacBook-Air.local>
2023-03-27 00:51:35 +0530

    Updates

---
+++ precache-manifest.39a5f89a0847e2cd16ec60d6e83dd727.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef4b4e23b2e4dc658f21",
+    "url": "/Textutils/static/css/main.37c69dc2.chunk.css"
+  },
+  {
+    "revision": "ef4b4e23b2e4dc658f21",
+    "url": "/Textutils/static/js/main.ef4b4e23.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/Textutils/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/Textutils/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "fa4348f6a4b91f9df22e",
+    "url": "/Textutils/static/js/runtime~main.fa4348f6.js"
+  },
+  {
+    "revision": "8f230e08184f7db0da24ce3900fdce93",
+    "url": "/Textutils/index.html"
+  }
+];
